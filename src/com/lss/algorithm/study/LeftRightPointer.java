f3398000ba9ca44e9852c841f1804f5950d65b77
li.shensong <lishensong@126.com>
2022-04-23 08:00:29 +0800

    左右指针实现翻转

--- src/com/lss/algorithm/study/LeftRightPointer.java
+++ src/com/lss/algorithm/study/LeftRightPointer.java
@@ -43,6 +43,18 @@ public class LeftRightPointer {
         return new int[]{-1,-1};
     }
 
+    public static void reverse(int[] nums){
+        int left = 0;
+        int right = nums.length - 1;
+        while (left < right){
+            int temp = nums[left];
+            nums[left] = nums[right];
+            nums[right] = temp;
+            left ++ ;
+            right --;
+        }
+    }
+
     public static void main(String[] args) {
         int[] nums = new int[] {1,3,9,12,17,30};
 
@@ -51,6 +63,11 @@ public class LeftRightPointer {
 
         int[] result = twoSum(nums,26);
         for (int num : result){
+            System.out.println(num);
+        }
+
+        reverse(nums);
+        for (int num : nums){
             System.out.println(num);
         }
     }
